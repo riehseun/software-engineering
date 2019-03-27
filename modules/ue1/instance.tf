f423c708584dd1c592386c664c7577a5a8628409
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-03-27 19:49:58 -0400

    update

--- modules/ue1/instance.tf
+++ modules/ue1/instance.tf
@@ -47,158 +47,158 @@ resource "google_compute_instance" "vm1" {
     }
 }
 
-# resource "google_compute_instance" "vm2" {
-#     name          = "k8s-node1"
-#     machine_type  = "n1-standard-1"
-#     zone          = "${format("%s","${var.var_region_name}-c")}"
+resource "google_compute_instance" "vm2" {
+    name          = "k8s-node1"
+    machine_type  = "n1-standard-1"
+    zone          = "${format("%s","${var.var_region_name}-c")}"
 
-#     tags          = ["ssh","http"]
+    tags          = ["ssh","http"]
 
-#     boot_disk {
-#         initialize_params {
-#             image = "centos-7-v20180129"
-#         }
-#     }
+    boot_disk {
+        initialize_params {
+            image = "centos-7-v20180129"
+        }
+    }
 
-#     metadata {
-#         foo = "bar"
-#     }
+    metadata {
+        foo = "bar"
+    }
 
-#     network_interface {
-#         subnetwork = "${google_compute_subnetwork.public_subnet.name}"
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public_subnet.name}"
 
-#         access_config {
-#             // Ephemeral IP
-#         }
-#     }
+        access_config {
+            // Ephemeral IP
+        }
+    }
 
-#     # provisioner "remote-exec" {
-#     #     inline = [
-#     #         "sudo -i",
-#     #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
-#     #         "yum update",
-#     #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
-#     #         "python3.6 -V"
-#     #     ]
+    # provisioner "remote-exec" {
+    #     inline = [
+    #         "sudo -i",
+    #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
+    #         "yum update",
+    #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
+    #         "python3.6 -V"
+    #     ]
 
-#     #     connection {
-#     #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #         type        = "ssh"
-#     #         user        = "${var.ssh_user}"
-#     #         private_key = "${file("~/.ssh/id_rsa")}"
-#     #     }
-#     # }
+    #     connection {
+    #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
+    #         type        = "ssh"
+    #         user        = "${var.ssh_user}"
+    #         private_key = "${file("~/.ssh/id_rsa")}"
+    #     }
+    # }
 
-#     # provisioner "local-exec" {
-#     #     environment {
-#     #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #     }
+    # provisioner "local-exec" {
+    #     environment {
+    #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
+    #     }
 
-#     #     working_dir = "../../../ansible/"
-#     #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
-#     # }
-# }
+    #     working_dir = "../../../ansible/"
+    #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
+    # }
+}
 
-# resource "google_compute_instance" "vm3" {
-#     name          = "k8s-node2"
-#     machine_type  = "n1-standard-1"
-#     zone          = "${format("%s","${var.var_region_name}-c")}"
+resource "google_compute_instance" "vm3" {
+    name          = "k8s-node2"
+    machine_type  = "n1-standard-1"
+    zone          = "${format("%s","${var.var_region_name}-c")}"
 
-#     tags          = ["ssh","http"]
+    tags          = ["ssh","http"]
 
-#     boot_disk {
-#         initialize_params {
-#             image = "centos-7-v20180129"
-#         }
-#     }
+    boot_disk {
+        initialize_params {
+            image = "centos-7-v20180129"
+        }
+    }
 
-#     metadata {
-#         foo = "bar"
-#     }
+    metadata {
+        foo = "bar"
+    }
 
-#     network_interface {
-#         subnetwork = "${google_compute_subnetwork.public_subnet.name}"
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public_subnet.name}"
 
-#         access_config {
-#             // Ephemeral IP
-#         }
-#     }
+        access_config {
+            // Ephemeral IP
+        }
+    }
 
-#     # provisioner "remote-exec" {
-#     #     inline = [
-#     #         "sudo -i",
-#     #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
-#     #         "yum update",
-#     #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
-#     #         "python3.6 -V"
-#     #     ]
+    # provisioner "remote-exec" {
+    #     inline = [
+    #         "sudo -i",
+    #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
+    #         "yum update",
+    #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
+    #         "python3.6 -V"
+    #     ]
 
-#     #     connection {
-#     #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #         type        = "ssh"
-#     #         user        = "${var.ssh_user}"
-#     #         private_key = "${file("~/.ssh/id_rsa")}"
-#     #     }
-#     # }
+    #     connection {
+    #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
+    #         type        = "ssh"
+    #         user        = "${var.ssh_user}"
+    #         private_key = "${file("~/.ssh/id_rsa")}"
+    #     }
+    # }
 
-#     # provisioner "local-exec" {
-#     #     environment {
-#     #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #     }
+    # provisioner "local-exec" {
+    #     environment {
+    #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
+    #     }
 
-#     #     working_dir = "../../../ansible/"
-#     #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
-#     # }
-# }
+    #     working_dir = "../../../ansible/"
+    #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
+    # }
+}
 
-# resource "google_compute_instance" "vm4" {
-#     name          = "k8s-node3"
-#     machine_type  = "n1-standard-1"
-#     zone          = "${format("%s","${var.var_region_name}-c")}"
+resource "google_compute_instance" "vm4" {
+    name          = "k8s-node3"
+    machine_type  = "n1-standard-1"
+    zone          = "${format("%s","${var.var_region_name}-c")}"
 
-#     tags          = ["ssh","http"]
+    tags          = ["ssh","http"]
 
-#     boot_disk {
-#         initialize_params {
-#             image = "centos-7-v20180129"
-#         }
-#     }
+    boot_disk {
+        initialize_params {
+            image = "centos-7-v20180129"
+        }
+    }
 
-#     metadata {
-#         foo = "bar"
-#     }
+    metadata {
+        foo = "bar"
+    }
 
-#     network_interface {
-#         subnetwork = "${google_compute_subnetwork.public_subnet.name}"
+    network_interface {
+        subnetwork = "${google_compute_subnetwork.public_subnet.name}"
 
-#         access_config {
-#             // Ephemeral IP
-#         }
-#     }
+        access_config {
+            // Ephemeral IP
+        }
+    }
 
-#     # provisioner "remote-exec" {
-#     #     inline = [
-#     #         "sudo -i",
-#     #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
-#     #         "yum update",
-#     #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
-#     #         "python3.6 -V"
-#     #     ]
+    # provisioner "remote-exec" {
+    #     inline = [
+    #         "sudo -i",
+    #         "yum install -y https://centos7.iuscommunity.org/ius-release.rpm",
+    #         "yum update",
+    #         "yum install -y python36u python36u-libs python36u-devel python36u-pip",
+    #         "python3.6 -V"
+    #     ]
 
-#     #     connection {
-#     #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #         type        = "ssh"
-#     #         user        = "${var.ssh_user}"
-#     #         private_key = "${file("~/.ssh/id_rsa")}"
-#     #     }
-#     # }
+    #     connection {
+    #         host        = "${self.network_interface.0.access_config.0.nat_ip}"
+    #         type        = "ssh"
+    #         user        = "${var.ssh_user}"
+    #         private_key = "${file("~/.ssh/id_rsa")}"
+    #     }
+    # }
 
-#     # provisioner "local-exec" {
-#     #     environment {
-#     #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
-#     #     }
+    # provisioner "local-exec" {
+    #     environment {
+    #         PUBLIC_IP  = "${self.network_interface.0.access_config.0.nat_ip}"
+    #     }
 
-#     #     working_dir = "../../../ansible/"
-#     #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
-#     # }
-# }
+    #     working_dir = "../../../ansible/"
+    #     command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-node.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
+    # }
+}
