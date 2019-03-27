4889cfc8a86f348381cadba9f717bdd611583bb7
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-03-27 17:44:52 -0400

    update

--- modules/ue1/instance.tf
+++ modules/ue1/instance.tf
@@ -47,8 +47,8 @@ resource "google_compute_instance" "vm1" {
         }
 
         working_dir = "../../../ansible/"
-        # command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-master.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
-        command     = "ansible-playbook -u root --private-key "${file("~/.ssh/id_rsa")}" k8s-master.yaml -i ${self.ipv4_address},"
+        command     = "ansible-playbook -u root --private-key ${var.ssh_key_private} k8s-master.yaml -i ${self.network_interface.0.access_config.0.nat_ip},"
+        # command     = "ansible-playbook -u root --private-key "${file("~/.ssh/id_rsa")}" k8s-master.yaml -i ${self.ipv4_address},"
     }
 }
 
