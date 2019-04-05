b88d1a2f21ea8da6142d9d074132e592e1051ad1
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-04-05 19:53:35 -0400

    update

--- modules/ue1/outputs.tf
+++ modules/ue1/outputs.tf
@@ -6,14 +6,14 @@ output "vm1_public_address" {
 	value = "${google_compute_instance.vm1.*.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "vm2_public_address" {
-    value = "${google_compute_instance.vm2.*.network_interface.0.access_config.0.nat_ip}"
-}
+# output "vm2_public_address" {
+#     value = "${google_compute_instance.vm2.*.network_interface.0.access_config.0.nat_ip}"
+# }
 
-output "vm3_public_address" {
-    value = "${google_compute_instance.vm3.*.network_interface.0.access_config.0.nat_ip}"
-}
+# output "vm3_public_address" {
+#     value = "${google_compute_instance.vm3.*.network_interface.0.access_config.0.nat_ip}"
+# }
 
-output "vm4_public_address" {
-    value = "${google_compute_instance.vm4.*.network_interface.0.access_config.0.nat_ip}"
-}
+# output "vm4_public_address" {
+#     value = "${google_compute_instance.vm4.*.network_interface.0.access_config.0.nat_ip}"
+# }
