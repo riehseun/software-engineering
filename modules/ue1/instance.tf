327f845531a061708b962ffe01af1409db4e7b32
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-04-05 19:26:44 -0400

    update

--- modules/ue1/instance.tf
+++ modules/ue1/instance.tf
@@ -47,7 +47,7 @@ resource "google_compute_instance" "vm1" {
 
         # You must install "ansible" on the machine where terraform-ansible suites get executed
         # Add "-vvv" for verbose output
-        command     = "ansible-playbook -vvv -u ${var.ssh_user} --private-key ~/.ssh/id_rsa k8s-master.yaml -i $PUBLIC_IP,"
+        command     = "ansible-playbook -u ${var.ssh_user} --private-key ~/.ssh/id_rsa k8s-master.yaml -i $PUBLIC_IP,"
     }
 }
 
@@ -97,7 +97,7 @@ resource "google_compute_instance" "vm2" {
             K8S_MASTER_IP             ="${google_compute_instance.vm1.network_interface.0.access_config.0.nat_ip}"
         }
 
-        command     = "ansible-playbook -vvv -u ${var.ssh_user} --private-key ~/.ssh/id_rsa k8s-node.yaml -i $PUBLIC_IP,"
+        command     = "ansible-playbook -u ${var.ssh_user} --private-key ~/.ssh/id_rsa k8s-node.yaml -i $PUBLIC_IP,"
     }
 }
 
