a64d0b5dfa69fcae054e1f2a185227a4c6e38b3d
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-04-05 22:38:46 -0400

    update

--- modules/ue1/instance.tf
+++ modules/ue1/instance.tf
@@ -97,7 +97,7 @@ resource "google_compute_instance" "vm2" {
             K8S_MASTER_IP             ="${google_compute_instance.vm1.network_interface.0.access_config.0.nat_ip}"
         }
 
-        command     = "ansible-playbook -u ${var.ssh_user} --private-key ~/.ssh/id_rsa ../ansible/k8s-node.yaml -i $PUBLIC_IP,"
+        command     = "ansible-playbook -vvv -u ${var.ssh_user} --private-key ~/.ssh/id_rsa ../ansible/k8s-node.yaml -i $PUBLIC_IP,"
     }
 }
 
