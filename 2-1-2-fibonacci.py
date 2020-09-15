17c10f682bbf5326967c23359984f810d8da8a84
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-15 17:50:34 -0400

    update

---
+++ 2-1-2-fibonacci.py
@@ -0,0 +1,35 @@
+# Uses python3
+def calc_fib(n):
+	"""
+	Implments efficient algorithm for finding nth number in Fibonacci
+	sequence
+
+	Args:
+	n -- specifies which Fibonacci number to find
+
+	Returns:
+	number -- nth number of Fibonacci sequence
+	"""
+
+	# Handle corner cases
+	if n == 0:
+		return 0
+	elif n == 1:
+		return 1
+
+	# Initialize array to store Fibonacci sequence
+	fib_array = []
+	for i in range(n):
+		fib_array.append(0)
+
+	fib_array[0] = 1
+	fib_array[1] = 1
+	for i in range(2,n):
+		fib_array[i] = fib_array[i-1] + fib_array[i-2]
+
+	number = fib_array[n-1]
+	return number
+
+
+n = int(input())
+print(calc_fib(n))
---
+++ 2-1-2-fibonacci_last_digit.py
@@ -0,0 +1,36 @@
+# Uses python3
+import sys
+
+def get_fibonacci_last_digit_naive(n):
+    """
+    Implments efficient algorithm for finding the last digit of nth
+    number in Fibonacci sequence
+
+    Args:
+    n -- specifies which Fibonacci number to find
+
+    Returns:
+    number -- last digit of nth number of Fibonacci sequence
+    """
+
+    if n <= 1:
+        return n
+
+    # Initialize array to store Fibonacci sequence
+    fib_array = []
+    for i in range(n):
+        fib_array.append(0)
+
+    fib_array[0] = 1
+    fib_array[1] = 1
+    for i in range(2,n):
+        fib_array[i] = (fib_array[i-1] + fib_array[i-2]) % 10
+
+    number = fib_array[n-1]
+    return number
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    print(get_fibonacci_last_digit_naive(n))
