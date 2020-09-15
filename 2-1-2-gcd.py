7fde710f6e55e94442a332f00adc3cfc554e992f
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-15 18:08:23 -0400

    update

--- 2-1-2-gcd.py
+++ 2-1-2-gcd.py
@@ -2,13 +2,24 @@
 import sys
 
 def gcd_naive(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
+    """
+    Implments efficient algorithm for finding the gretest common divisor
 
-    return current_gcd
+    Args:
+    a -- numerator
+    b -- denominator
+
+    Returns:
+    gretest common divisor
+    """
+
+    if b == 0:
+        return a
+
+    c = a % b
+
+    return gcd_naive(b, c)
+
 
 if __name__ == "__main__":
     input = sys.stdin.read()
--- 2-1-2-lcm.py
+++ 2-1-2-lcm.py
@@ -2,14 +2,33 @@
 import sys
 
 def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+	"""
+	Finds the least common multiple
 
-    return a*b
+	Args:
+	a -- first number
+	b -- second number
+
+	Returns:
+	least common multiple
+	"""
+
+	# Swap such that "b" is always greater than "a"
+    if a > b:
+    	temp = a
+    	a = b
+    	b = temp
+
+    i = 1
+    while i <= a:
+    	if b * i % a == 0:
+    		return b * i
+    	i += 1
+
+    return 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
     print(lcm_naive(a, b))
-
