edf4b7b3cee4315bde9edd5bdd84228cf2576008
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-19 14:11:48 -0400

    update

--- 2-1-3-car_fueling.py
+++ 2-1-3-car_fueling.py
@@ -3,9 +3,44 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    """
+	Solves classical Car fueling problem using greedy algorithm
 
-if __name__ == '__main__':
-    d, m, _, *stops = map(int, sys.stdin.read().split())
-    print(compute_min_refills(d, m, stops))
+    Args:
+    distance -- total distance to travel
+    tank -- distance that the car can travel w/o fueling
+    stops -- array representing the location of gas stations
+
+    Returns:
+    num_refills -- the mimimum number of refills required
+    """
+
+    num_refills = 0
+    current_refill = 0
+
+    all_stops = []
+    all_stops.append(0)
+    for stop in stops:
+    	all_stops.append(stop)
+    all_stops.append(distance)
+
+    num_stops = len(all_stops)
+
+    while current_refill < num_stops:
+    	last_refill = current_refill
+
+    	while (current_refill < num_stops and all_stops[current_refill+1] - all_stops[last_refill] <= tank):
+    		current_refill += 1
+    		if current_refill == num_stops-1:
+    			return num_refills
+
+    	if current_refill == last_refill:
+    		return -1
+    	if current_refill < num_stops:
+    		num_refills += 1
+
+    return num_refills
+
+
+print(compute_min_refills(950, 400, [200, 375, 550, 750]))
+print(compute_min_refills(10, 3, [1, 2, 5, 9]))
