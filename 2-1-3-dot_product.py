e934bab9ba572154025fca7d29a72a30780096be
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-19 14:47:19 -0400

    update

--- 2-1-3-dot_product.py
+++ 2-1-3-dot_product.py
@@ -3,17 +3,40 @@
 import sys
 
 def max_dot_product(a, b):
-    #write your code here
-    res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+    """
+    Calculates the maximum possible product from the elements of two
+    input lists
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    a = data[1:(n + 1)]
-    b = data[(n + 1):]
-    print(max_dot_product(a, b))
-    
+    Args:
+    a -- first list
+    b -- second list
+
+    Returns:
+    result -- the maximum possible product
+    """
+
+    result = 0
+
+    a_sorted = sorted(a, reverse=True)
+    b_sorted = sorted(b, reverse=True)
+
+    for i in range(len(a)):
+        if a_sorted[i] > 0 and b_sorted[i] > 0:
+            result += a_sorted[i] * b_sorted[i]
+        else:
+            break
+
+    a_sorted = sorted(a)
+    b_sorted = sorted(b)
+
+    for i in range(len(a)):
+        if a_sorted[i] < 0 and b_sorted[i] < 0:
+            result += a_sorted[i] * b_sorted[i]
+        else:
+            break
+
+    return result
+
+
+print(max_dot_product([1, 3, -5], [-2, 4, 1]))
+print(max_dot_product([1, 3, 5], [2, 4, 1]))
