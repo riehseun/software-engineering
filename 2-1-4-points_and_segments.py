946c1a7eae2e00380a369f0c05ba1c247f67ef34
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-26 22:54:46 -0400

    update

--- 2-1-4-points_and_segments.py
+++ 2-1-4-points_and_segments.py
@@ -1,18 +1,54 @@
 # Uses python3
 import sys
 
-def fast_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
-    #write your code here
-    return cnt
 
-def naive_count_segments(starts, ends, points):
-    cnt = [0] * len(points)
-    for i in range(len(points)):
-        for j in range(len(starts)):
-            if starts[j] <= points[i] <= ends[j]:
-                cnt[i] += 1
-    return cnt
+def count_segments(starts, ends, points):
+    """
+
+    """
+
+    count = 0
+    data = {}
+    answer = {}
+    string = ""
+
+    for elem in starts:
+        data[elem] = "L"
+
+    for elem in ends:
+        data[elem] = "R"
+
+    for elem in points:
+        data[elem] = "P"
+
+    # sort by key, then by value (so that left coordinate comes before the point)
+    sorted_data = sorted(data.items(), key=lambda x: (x[0],x[1]), reverse=False)
+
+    for key, value in sorted_data:
+        if value == "L":
+            count += 1
+        elif value == "R":
+            count -= 1
+        else:
+            if count < 0:
+                answer[key] = 0
+            else:
+                answer[key] = count
+            count = 0
+
+    for point in points:
+        string += str(answer[point])
+        string += " "
+
+    return string[:-1]
+
+
+# print(count_segments([0,7], [5,10], [1,6,11]))
+# print(count_segments([-10], [10], [-100,100,0]))
+# print(count_segments([0,-3,7], [5,2,10], [1,6]))
+
+
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -23,6 +59,6 @@ if __name__ == '__main__':
     ends   = data[3:2 * n + 2:2]
     points = data[2 * n + 2:]
     #use fast_count_segments
-    cnt = naive_count_segments(starts, ends, points)
+    cnt = count_segments(starts, ends, points)
     for x in cnt:
         print(x, end=' ')
