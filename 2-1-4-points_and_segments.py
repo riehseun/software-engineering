ba355befdbb78ba32410cf4e3b803c35624bc5b8
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-09-27 21:00:28 -0400

    update

--- 2-1-4-points_and_segments.py
+++ 2-1-4-points_and_segments.py
@@ -4,37 +4,46 @@ import sys
 
 def count_segments(starts, ends, points):
     """
+    For each point, find the number of segments that contain this point
 
+    Args:
+    starts -- list of starting points of segments
+    ends -- list of ending points of segments
+    points -- points to check
+
+    Returns:
+    the number of segments that contain each point given in the input
+    list "points"
     """
 
     count = 0
-    data = {}
+    data = []
     answer = {}
     string = ""
 
     for elem in starts:
-        data[elem] = "L"
+        data.append([elem, "L"])
 
     for elem in ends:
-        data[elem] = "R"
+        data.append([elem, "R"])
 
     for elem in points:
-        data[elem] = "P"
+        data.append([elem, "P"])
 
-    # sort by key, then by value (so that left coordinate comes before the point)
-    sorted_data = sorted(data.items(), key=lambda x: (x[0],x[1]), reverse=False)
+    # sort by the first item, then by type (so that left coordinate
+    # comes before the point)
+    sorted_data = sorted(data, key=lambda x: (x[0],x[1]), reverse=False)
 
-    for key, value in sorted_data:
-        if value == "L":
+    for item in sorted_data:
+        if item[1] == "L":
             count += 1
-        elif value == "R":
+        elif item[1] == "R":
             count -= 1
-        else:
+        elif item[1] == "P":
             if count < 0:
-                answer[key] = 0
+                answer[item[0]] = 0
             else:
-                answer[key] = count
-            count = 0
+                answer[item[0]] = count
 
     for point in points:
         string += str(answer[point])
@@ -43,22 +52,6 @@ def count_segments(starts, ends, points):
     return string[:-1]
 
 
-# print(count_segments([0,7], [5,10], [1,6,11]))
-# print(count_segments([-10], [10], [-100,100,0]))
-# print(count_segments([0,-3,7], [5,2,10], [1,6]))
-
-
-
-
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    m = data[1]
-    starts = data[2:2 * n + 2:2]
-    ends   = data[3:2 * n + 2:2]
-    points = data[2 * n + 2:]
-    #use fast_count_segments
-    cnt = count_segments(starts, ends, points)
-    for x in cnt:
-        print(x, end=' ')
+print(count_segments([0,7], [5,10], [1,6,11]))
+print(count_segments([-10], [10], [-100,100,0]))
+print(count_segments([0,-3,7], [5,2,10], [1,6]))
