6cde2ec86a80054ebaaee106abe175a47675ee5a
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2020-11-22 14:48:59 -0500

    update

--- 2-1-6-knapsack.py
+++ 2-1-6-knapsack.py
@@ -1,15 +1,41 @@
-# Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    W, n, *w = list(map(int, input.split()))
-    print(optimal_weight(W, w))
+def optimal_weight(W, weights):
+    """
+    Solving classical knapsack problem where weights == values and
+    same items cannot be drawn more than once
+
+    Args:
+    W -- total weight
+    weights -- list of all available weights
+
+    Returns:
+    Optimal value
+    """
+
+
+    # In this problem, the values and weights are the same
+    n = len(weights)
+    v = [[0 for i in range(n+1)] for w in range(W+1)]
+
+    # Initialize values
+    for i in range(n+1):
+        v[0][i] = 0
+
+    for w in range(W+1):
+        v[w][0] = 0
+
+    for i in range(1, n+1):
+        for w in range (1, W+1):
+            v[w][i] = v[w][i-1]
+
+            if weights[i-1] <= w:
+                val = v[w-weights[i-1]][i-1] + weights[i-1]
+                if v[w][i] < val:
+                    v[w][i] = val
+
+    return v[W][n]
+
+
+print(optimal_weight(10, [1,4,8]))
