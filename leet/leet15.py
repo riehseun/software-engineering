bf4319ce8c739ba60adbcf787dbc48230e01893e
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-05-05 16:50:30 -0400

    update

--- leet/leet15.py
+++ leet/leet15.py
@@ -27,35 +27,39 @@ class Leet15(object):
         # Sorted: [0,0,0]
         #          s   e
 
-        nums.sort()
+        # [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
+
+        nums.sort()  # O(nlogn)
 
         for i in range(len(nums)):
             start = 0
             end = len(nums) - 1
             while end > start:
-                if (i != start
-                    and i != end
-                    and nums[i] + nums[start] + nums[end] == 0):
-                    # All 3 conditions satisfied.
-
-                    new = True
-                    for triplet in triplets:
-                        # Triplet does not already exist.
-                        if (nums[i] in triplet
-                            and nums[start] in triplet
-                            and nums[end] in triplet):
-                            new = False
-                    if new:
-                        triplets.append([nums[i],nums[start],nums[end]])
-
                 # If the sum is less than zero, need to increment the
                 # start index to increase the sum.
                 if nums[i] + nums[start] + nums[end] < 0:
                     start += 1
                 # If the sum is greater than zero, need to decrement the
                 # end index to decrease the sum.
-                else:
+                elif nums[i] + nums[start] + nums[end] > 0:
                     end -= 1
+                else:
+                    print("i:"+str(nums[i]) + " start:"+str(nums[start]) + " end:"+str(nums[end]))
+                    if i != end and i != start:
+                        new = True
+                        for triplet in triplets:
+                            # Triplet already exists.
+                            if (nums[i] in triplet
+                                and nums[start] in triplet
+                                and nums[end] in triplet):
+                                new = False
+                        if new:
+                            print(str([nums[i],nums[start],nums[end]]))
+                            triplets.append([nums[i],nums[start],nums[end]])
+                    if nums[start] <= nums[end]:
+                        start += 1
+                    else:
+                        end -= 1
 
             # start = 0
             # end = len(nums) - 1
