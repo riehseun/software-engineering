9f5f13e020284546572cf2cbaea6103d0392924b
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-05-29 15:01:07 -0400

    update

--- leet/leet88.py
+++ leet/leet88.py
@@ -6,8 +6,42 @@
 class Leet88(object):
     def merge(self, nums1, m, nums2, n):
         """
+        Merge two sorted arrays into one sorted array.
 
+        Args:
+            nums1 -- A sorted array in non-decreasing order.
+            nums2 -- A sorted array in non-decreasing order.
+            m -- Length of nums1.
+            n -- Length of nums2.
+
+        Returns:
+            nums1 which is the sorted array combining nums1 and nums2.
         """
 
-        return
+        # Example: [2,0], 1, [1], 1
+        # [2,0] m=1 n=1
+        # [2,2] m=0 n=1
+        # n > 0 => [1,2]
+
+        if n == 0:
+            return nums1
+        if m == 0:
+            for i in range(len(nums2)):
+                nums1[i] = nums2[i]
+            return nums1
+
+        # Time: O(m+n)
+        # Space: O(1)
+        while n > 0 and m > 0:
+            if nums2[n-1] > nums1[m-1]:
+                nums1[n+m-1] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[n+m-1] = nums1[m-1]
+                m -= 1
+
+        if n > 0:
+            nums1[:n] = nums2[:n]
+
+        return nums1
 
--- leet/leet88_test.py
+++ leet/leet88_test.py
@@ -11,6 +11,22 @@ class TestLeet88(unittest.TestCase):
         test = self.leet88.merge([1,2,3,0,0,0], 3, [2,5,6], 3)
         self.assertEqual(test, [1,2,2,3,5,6])
 
+    def test_merge_2(self):
+        test = self.leet88.merge([1], 1, [], 0)
+        self.assertEqual(test, [1])
+
+    def test_merge_3(self):
+        test = self.leet88.merge([0], 0, [1], 1)
+        self.assertEqual(test, [1])
+
+    def test_merge_4(self):
+        test = self.leet88.merge([1,0], 1, [2], 1)
+        self.assertEqual(test, [1,2])
+
+    def test_merge_5(self):
+        test = self.leet88.merge([2,0], 1, [1], 1)
+        self.assertEqual(test, [1,2])
+
 
 if __name__ == "__main__":
     unittest.main()
