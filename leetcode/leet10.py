39272e4093f30745c14395723f1afa13bdde0252
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2022-06-24 18:37:28 -0400

    update

--- leetcode/leet10.py
+++ leetcode/leet10.py
@@ -7,6 +7,8 @@ class Leet10(object):
     def is_match(self, s, p):
         """
         Implements regular expression matching.
+        * indicates "zero or more" of a preceeding element.
+        . indicates any single character.
 
         Args:
             s -- An input string.
@@ -15,6 +17,18 @@ class Leet10(object):
         Returns:
             True if regular expression matches. False otherwise
         """
+
+        # Example
+        # s = "aab"
+        # p = "c*a*b"
+
+        # Y if substring in p matches substing in s. N otherwise.
+        #       c * a * b
+        #     0 1 2 3 4 5
+        #   0 Y N Y N Y N
+        # a 1 N N N Y Y N
+        # a 2 N N N N Y N
+        # b 3 N N N N N Y
 
 
 
